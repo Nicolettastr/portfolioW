16d76fcfd3b221c459575891ba02cb70d95ce8ea
Nicolettastr <nicolettastruggia@hotmail.com>
2025-04-21 00:31:54 +0200

    implementing responsive sizing changes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,70 +5,62 @@ import { MdOutlinePhonelink } from "react-icons/md";
 import MoreBtn from "./MoreBtn";
 
 const Work = () => {
-    const companies = [
-        {
-            name: "babel group",
-            img: "/babel.png",
-            link: "https://babelgroup.com/",
-            description:
-                "Global tech consultancy specializing in digital transformation through AI, cybersecurity, and cloud solutions",
-        },
-        {
-            name: "sice",
-            img: "/sice.png",
-            link: "https://www.sice.com/",
-            description:
-                "Multinational leader in integrating technologies for managing public infrastructure, specializing in mobility, transport, energy, and smart urban services. ",
-        },
-        {
-            name: "donair",
-            img: "/donair.png",
-            link: "https://donair.es/",
-            description:
-                "Family-owned company, specializing in a wide range of services like electrical work, cleaning, and air conditioning systems, with a focus on high-quality installations and maintenance.",
-        },
-    ];
+  const companies = [
+    {
+      name: "babel group",
+      img: "/babel.png",
+      link: "https://babelgroup.com/",
+      description:
+        "Global tech consultancy specializing in digital transformation through AI, cybersecurity, and cloud solutions",
+    },
+    {
+      name: "sice",
+      img: "/sice.png",
+      link: "https://www.sice.com/",
+      description:
+        "Multinational leader in integrating technologies for managing public infrastructure, specializing in mobility, transport, energy, and smart urban services. ",
+    },
+    {
+      name: "donair",
+      img: "/donair.png",
+      link: "https://donair.es/",
+      description:
+        "Family-owned company, specializing in a wide range of services like electrical work, cleaning, and air conditioning systems, with a focus on high-quality installations and maintenance.",
+    },
+  ];
 
-    return (
-        <section className='work'>
-            <h3 className='gl-title'>Highlights from the Road</h3>
-            <div className='logo_container w-100 d-flex d-column justify-center align-center'>
-                {companies.map((company) => (
-                    <div className='card w-100'>
-                        <figure className='logo_wrapper w-100'>
-                            <img
-                                className='logo_img w-100'
-                                src={company.img}
-                                alt={company.img}
-                            />
-                            <span></span>
-                        </figure>
-                        <div className='company-info w-100'>
-                            <h3>{company.name.toUpperCase()}</h3>
-                            <p>{company.description}</p>
-                            <a
-                                className='work_icon'
-                                target='_blank'
-                                href={company.link}
-                            >
-                                <SiLinkedin />
-                            </a>
-                            <a
-                                className='work_icon'
-                                target='_blank'
-                                href={company.link}
-                            >
-                                <MdOutlinePhonelink />
-                            </a>
-                        </div>
-                    </div>
-                ))}
+  return (
+    <section className="work">
+      <h3 className="gl-title">Highlights from the Road</h3>
+      <div className="logo_container w-100">
+        {companies.map((company) => (
+          <div className="card w-100">
+            <figure className="logo_wrapper w-100">
+              <img
+                className="logo_img w-100"
+                src={company.img}
+                alt={company.img}
+              />
+              <span></span>
+            </figure>
+            <div className="company-info w-100">
+              <h3>{company.name.toUpperCase()}</h3>
+              <p>{company.description}</p>
+              <a className="work_icon" target="_blank" href={company.link}>
+                <SiLinkedin />
+              </a>
+              <a className="work_icon" target="_blank" href={company.link}>
+                <MdOutlinePhonelink />
+              </a>
             </div>
-            <div className='btn_wrapper'>
-                <MoreBtn />
-            </div>
-        </section>
-    );
+          </div>
+        ))}
+      </div>
+      <div className="btn_wrapper">
+        <MoreBtn />
+      </div>
+    </section>
+  );
 };
 
 export default Work;
